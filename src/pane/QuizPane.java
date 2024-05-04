0aaafad73a4eb56c4dbe3e5019cd31414e05d2f6
VickyManCH <kittinatvick@gmail.com>
2024-05-04 10:37:02 +0700

    Added quizPage

--- src/pane/QuizPane.java
+++ src/pane/QuizPane.java
@@ -1,9 +1,26 @@
 package pane;
 
-import javafx.scene.layout.VBox;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class QuizPane extends VBox {
-    public QuizPane(){
+    public QuizPane(String question){
         setSpacing(16);
+        setPadding(new Insets(32,0,32,0));
+        Button quizFrame = new Button(question);
+        quizFrame.setBackground(new Background(new BackgroundFill(Color.WHITE,new CornerRadii(20),null)));
+        quizFrame.setPrefWidth(1080);
+        quizFrame.setPrefHeight(540);
+        setAlignment(Pos.CENTER);
+        getChildren().add(quizFrame);
     }
 }
--- src/utils/Goto.java
+++ src/utils/Goto.java
@@ -85,6 +85,7 @@ public class Goto {
 
     public static void quizPage(){
         clear();
+        rootPane.getChildren().add(new QuizPane("How is Arif doing?"));
     }
 
     public static void titleScreenPage(){
@@ -95,13 +96,28 @@ public class Goto {
         exitButton.setTextFill(Color.DARKCYAN);
         exitButton.setPrefWidth(300);
         exitButton.setPrefHeight(100);
+        exitButton.setOpacity(0.5);
         exitButton.setOnMouseClicked(mouseEvent -> Platform.exit());
         Button playButton = new Button("Play");
         playButton.setBorder(new Border(new BorderStroke(Color.DARKCYAN,BorderStrokeStyle.SOLID,null,new BorderWidths(2))));
-        playButton.setBackground(new Background(new BackgroundFill(Color.WHITE,null,null)));
+        playButton.setBackground(new Background(new BackgroundFill(Color.WHITE,new CornerRadii(16),null)));
         playButton.setTextFill(Color.DARKCYAN);
         playButton.setPrefWidth(300);
         playButton.setPrefHeight(100);
+        playButton.setOnMouseExited(mouseEvent -> {
+            playButton.setBorder(new Border(new BorderStroke(Color.DARKCYAN,BorderStrokeStyle.SOLID,null,new BorderWidths(2))));
+            playButton.setBackground(new Background(new BackgroundFill(Color.WHITE,null,null)));
+            playButton.setTextFill(Color.DARKCYAN);
+            playButton.setPrefWidth(300);
+            playButton.setPrefHeight(100);
+        });
+        playButton.setOnMouseEntered(mouseEvent -> {
+            playButton.setBorder(new Border(new BorderStroke(Color.GREEN,BorderStrokeStyle.SOLID,null,new BorderWidths(2))));
+            playButton.setBackground(new Background(new BackgroundFill(Color.BLACK,null,null)));
+            playButton.setTextFill(Color.WHITE);
+            playButton.setPrefWidth(300);
+            playButton.setPrefHeight(100);
+        });
         playButton.setOnMouseClicked(mouseEvent -> quizPage());
         VBox.setMargin(playButton, new Insets(300,0,50,0));
         rootPane.getChildren().add(playButton);
