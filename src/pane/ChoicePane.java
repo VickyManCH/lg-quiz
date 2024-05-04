1e8d57164a001e352e59955c9dd1646f48fb8dcf
vichayabhas <6633227421@student.chula.ac.th>
2024-05-04 12:15:27 +0700

    Update ChoicePane.java

--- src/pane/ChoicePane.java
+++ src/pane/ChoicePane.java
@@ -1,4 +1,35 @@
 package pane;
 
-public class ChoicePane {
+import javafx.scene.control.Button;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.GridPane;
+
+import java.util.ArrayList;
+//
+public class ChoicePane extends GridPane {
+    public  ChoicePane(ArrayList<String> choices){
+        //ArrayList<Button> buttons = new ArrayList<>();
+        int count=choices.size();
+        int w;
+        int h;
+        setMaxSize(600,150);
+
+        switch (count){
+            case(2):{
+                w=300;
+                h=150;
+
+
+            } case (4):{
+                w=300;
+                h=75;
+
+            }
+        }
+        for (String choice:choices){
+            Button button=new Button(choice);
+
+            getChildren().add(button);
+        }
+    }
 }
